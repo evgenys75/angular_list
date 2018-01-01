0308d35ccbe29dcaf05049c5756cca61ea115909
evgeniy <evgenysmrnv@gmail.com>
2018-01-01 18:41:35 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
-  selector: 'app-root',
+  selector: 'test_app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+    providers:[UsersService]
 })
 export class AppComponent {
-  title = 'app';
+
+  users=[];
+
+  constructor(private userService:UsersService){
+
+  }
+
+  ngOnInit(){
+
+      this.users=this.userService.users
+
+    }
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,9 @@
+export class UsersService{
+
+    users=[
+        {name: 'First Name', date:'2017-11-11',text:'Create user service that get User-Id from the model and return user image'},
+        {name: 'Second Name', date:'2017-11-12',text:'Create user service that get User-Id from the model and return user image<br/>Create user service that get User-Id from the model and return user image'},
+        {name: 'Full Name', date:'2017-12-11',text:'Create user service that get User-Id from the model and return'}
+    ]
+
+}
